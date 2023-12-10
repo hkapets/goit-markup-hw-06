9eac25c3a0ec31468777c348a0a67a51ef1d5d1e
hkapets <147987058+hkapets@users.noreply.github.com>
2023-12-10 11:18:26 +0200

    modal-script

--- js/modal.js
+++ js/modal.js
@@ -7,10 +7,8 @@
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-modal]"),
   };
-
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
